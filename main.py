7867d408367de00a82b528b72aa789e40c7595e3
beyondthwisdom <139395290+beyondthwisdom@users.noreply.github.com>
2024-06-22 14:51:37 +0000

    Python kodlama örnekleri 1.ders

---
+++ main.py
@@ -0,0 +1,47 @@
+print("------------String birleştirme---------------------")
+print("Sıcaklık:", 25, "derece")
+print(f"Toplam: {100 + 250} TL")
+print("Skor: " + str(85) + " puan")
+print("------------Matematiksel İşlemler---------------------")
+print("Toplama: ", 10 + 5)
+print("Çıkarma: ", 10 - 5)
+print("Çarpma: ", 10 * 5)
+print("Bölme: ", 10 / 5)
+print("Üs Alma: ", 2 ** 3)
+print("Mod Alma: ", 10 % 3)
+print("------------Detaylı Matematiksel İşlemler-----------------------")
+print("5 + 3 =", 5 + 3)
+print("10 - 4 =", 10 - 4)
+print("7 * 6 =", 7 * 6)
+print("8 / 2 =", 8 / 2)
+print("3^4 =", 3 ** 4)
+print("14 % 5 =", 14 % 5)
+print("----------------Değişken Kullanımı------------------")
+a = 5
+b = 7
+print(f"{a} sayısıyla {b} sayısının toplamı  = {a + b} dir")
+
+print("------------------Karmaşık Aritmetik İşlemler-----------------------")
+p = 3
+q = 4
+r = 5
+sonuc = (p + q) * r - (q / p) ** r
+print(f"({p} + {q}) * {r} - ({q} / {p}) ^ {r} = {sonuc}")
+print("------------------Reserved Word kullanımı-----------------------")
+"""
+False      await      else       import     pass
+None       break      except     in         raise
+True       class      finally    is         return
+and        continue   for        lambda     try
+as         def        from       nonlocal   while
+assert     del        global     not        with
+async      elif       if         or         yield
+"""
+
+
+
+
+
+
+
+
